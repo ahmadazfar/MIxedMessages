0ec974d7645ea0196247aaeb6f851b5d61ab16d9
ahmadazfar <ahmadazfar1999@gmail.com>
2023-05-13 17:59:49 +0800

    Create html, css, and edited js file

--- mix_message.js
+++ mix_message.js
@@ -1,4 +1,11 @@
-let phrases = ["Not the Sharpest Tool in the Shed", "Hard Pill to Swallow", "Up In Arms",'A Bite at the Cherry','Money Doesn\'t Grow On Trees','Down To The Wire','Playing For Keeps','A Leg Up','Close But No Cigar']
-let randnum = Math.floor(Math.random() * phrases.length)
+let phrases = ["Not the Sharpest Tool in the Shed", "Hard Pill to Swallow", "Up In Arms",'A Bite at the Cherry','Money Doesn\'t Grow On Trees','Down To The Wire','Playing For Keeps','A Leg Up','Close But No Cigar'];
+let randnum = Math.floor(Math.random() * phrases.length);
 
-console.log(phrases[randnum])
+
+let btn = document.querySelector('button');
+
+function changePhrase() {
+    document.querySelector('h1').innerHTML = phrases[randnum];
+}
+
+btn.onclick = changePhrase
