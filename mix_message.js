9e5793c37ff2bc81064258261c47151cf4aa7dda
ahmadazfar <ahmadazfar1999@gmail.com>
2023-05-13 21:15:08 +0800

    made nicer, added more css

--- mix_message.js
+++ mix_message.js
@@ -1,11 +1,19 @@
 let phrases = ["Not the Sharpest Tool in the Shed", "Hard Pill to Swallow", "Up In Arms",'A Bite at the Cherry','Money Doesn\'t Grow On Trees','Down To The Wire','Playing For Keeps','A Leg Up','Close But No Cigar'];
-let randnum = Math.floor(Math.random() * phrases.length);
-
-
+let prev = ''
+let cur = ''
 let btn = document.querySelector('button');
 
 function changePhrase() {
-    document.querySelector('h1').innerHTML = phrases[randnum];
+
+    do
+    {
+    let randnum = Math.floor(Math.random() * phrases.length);
+    cur = phrases[randnum]
+    console.log(cur)
+    }while(prev === cur)
+    document.querySelector('h2').innerHTML = cur;
+    prev = cur
+
 }
 
 btn.onclick = changePhrase
