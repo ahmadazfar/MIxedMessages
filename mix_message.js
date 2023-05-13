a1d1810562654c67acf234cf02b146208417c35b
ahmadazfar <ahmadazfar1999@gmail.com>
2023-05-13 16:26:54 +0800

    Created js file

---
+++ mix_message.js
@@ -0,0 +1,4 @@
+let phrases = ["Not the Sharpest Tool in the Shed", "Hard Pill to Swallow", "Up In Arms",'A Bite at the Cherry','Money Doesn\'t Grow On Trees','Down To The Wire','Playing For Keeps','A Leg Up','Close But No Cigar']
+let randnum = Math.floor(Math.random() * phrases.length)
+
+console.log(phrases[randnum])
